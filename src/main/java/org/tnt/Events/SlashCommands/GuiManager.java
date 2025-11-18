a30c397698214cff256280706901635e8a67b887
yuratnt <starkov-07@list.ru>
2025-11-18 11:10:40 +0500

    localizaton manager add

--- src/main/java/org/tnt/Events/SlashCommands/GuiManager.java
+++ src/main/java/org/tnt/Events/SlashCommands/GuiManager.java
@@ -2,11 +2,13 @@ package org.tnt.Events.SlashCommands;
 
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.DiscordLocale;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import org.tnt.Localization.LocalizationManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 public class GuiManager extends ListenerAdapter {
 
     List<CommandData> commandData = new ArrayList<>();
+
 
     public void init(GenericGuildEvent event) {
         commandData.clear();
@@ -23,12 +26,58 @@ public class GuiManager extends ListenerAdapter {
     }
 
     private void otherCommands() {
+        LocalizationManager localizationRu = new LocalizationManager("RU", "other");
+        LocalizationManager localizationEN = new LocalizationManager("EN", "other");
 
-        commandData.add(Commands.slash("", "Содержит различные вспомогательные команды бота")
-                .addSubcommands(new SubcommandData("help", "Информация о боте и его основных командах"))
-                .addSubcommands(new SubcommandData("register", "Регистрирует ваш профиль на данном сервере для начала использования минигр"))
-                .addSubcommands(new SubcommandData("initialization", "Обязательная команды для работы бота. Устанавливает чат, в котором будут проводиться минигры")
-                        .addOptions(new OptionData(OptionType.CHANNEL, "chanel", "Чат, в котором будут проводиться минигры", true))
+
+        commandData.add(Commands.slash("settings", localizationEN.getDescription("settings"))
+                //Локализация
+                .setDescriptionLocalization(
+                        DiscordLocale.RUSSIAN,
+                        localizationRu.getDescription("settings")
+                )
+
+                //подкоманды
+                .addSubcommands(new SubcommandData(
+                        "help",
+                        localizationEN.getSubcommandDescription("settings", "help")
+                        )
+                        .setDescriptionLocalization(
+                                DiscordLocale.RUSSIAN,
+                                localizationRu.getSubcommandDescription("settings", "help")
+                        )
+                )
+                .addSubcommands(new SubcommandData(
+                        "register",
+                        localizationEN.getSubcommandDescription(
+                                "settings",
+                                "register"
+                        ))
+                        .setDescriptionLocalization(
+                                DiscordLocale.RUSSIAN,
+                                localizationRu.getSubcommandDescription(
+                                        "settings",
+                                        "register"
+                                ))
+                )
+                .addSubcommands(new SubcommandData(
+                        "initialization",
+                        localizationEN.getSubcommandDescription(
+                                "settings",
+                                "initialization"
+                        ))
+                        .setDescriptionLocalization(
+                                DiscordLocale.RUSSIAN,
+                                localizationRu.getSubcommandDescription(
+                                        "settings",
+                                        "initialization"
+                                ))
+                        .addOptions(new OptionData(
+                                OptionType.CHANNEL,
+                                "chanel",
+                                "Чат, в котором будут проводиться минигры",
+                                true
+                        ))
                 )
         );
     }
--- src/main/java/org/tnt/Localization/LocalizationManager.java
+++ src/main/java/org/tnt/Localization/LocalizationManager.java
@@ -2,6 +2,7 @@ package org.tnt.Localization;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.intellij.lang.annotations.Language;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,33 +17,39 @@ public class LocalizationManager {
         this.language = language;
         this.type = type;
     }
-    public String getTitle(String command) {
-        return getData(command, "title");
-    }
+
     public String getDescription(String command) {
-        return getData(command, "description");
+        return getData(command, null ,"description");
 
     }
-
-    public String getOption(String command, String option) {
-        return getData(command, "option", option);
+    public String getSubcommandDescription(String command, String subcommand) {
+        String[] subcommandPath = new String[] {
+                "subcommands",
+                "description"
+        };
+        return getData(command, subcommandPath, subcommand);
     }
 
-    private String getData(String command,String key) {
+    public String getCommandOption(String command, String option) {
+        String[] commandPath = new String[] {
+                "options"
+        };
+        return getData(command, commandPath, option);
+    }
+
+    private String getData(String command, String[] path, String key) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            JsonNode config = mapper.readTree(new File("Localization/RU/Command.json"));
-            return config.path(type).path(command).get(key).asText();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-    private String getData(String command, String path, String key) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            JsonNode config = mapper.readTree(new File("Localization/RU/Command.json"));
-            return config.path(type).path(path).path(command).get(key).asText();
+            JsonNode config = mapper.readTree(new File("src/main/resources/Localization/" + language + "/Command.json"));
+            config = config.path(type).path(command);
+            if (path != null) {
+                for (String s : path) {
+                    if (s != null && !s.isEmpty()) {
+                        config = config.path(s);
+                    }
+                }
+            }
+            return config.get(key).asText();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
